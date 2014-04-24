aeed9ffee5ea76ed63a749dc60610d0f39e30773
Ryan Williams <ryanw@iastate.edu>
2014-04-24 12:11:58 -0500

    parsing results

--- parsing_results_network_analysis.R
+++ parsing_results_network_analysis.R
@@ -14,10 +14,13 @@ body_results<-subset(results, trt=="M1"| trt=="M2"| trt=="M3" | trt=="M4" | trt=
 network_stats<-function(dataset){
 	final.results<-data.frame()
 	rhos<-c(-.75,-.5,-.25,.25,.5,.75)
+	trts<-as.vector(unique(dataset$trt))
+for(t in 1:length(trts)){
+	dataset_trt<-subset(dataset, trt==trts[t])
 for(r in 1:6){
 	#r<-4
-	if(rhos[r] < 0){temp<-subset(dataset, rho <= rhos[r])}
-	if(rhos[r] > 0){temp<-subset(dataset, rho >= rhos[r])}
+	if(rhos[r] < 0){temp<-subset(dataset_trt, rho <= rhos[r])}
+	if(rhos[r] > 0){temp<-subset(dataset_trt, rho >= rhos[r])}
 	#temp
 	#head(temp)
 	temp.graph<-simplify(graph.edgelist(as.matrix(temp[,c(2,3)]),directed=FALSE))
@@ -27,7 +30,10 @@ for(r in 1:6){
 	names(stats)<-c("order","norm_degree","betweenness")
 	row.names(stats)<-NULL
 	stats$rho_cut<-rhos[r]
+	stats$trt<-trts[t]
 	final.results<-rbind(final.results,stats)	
+}
+	
 }
 	return(final.results)
 }
